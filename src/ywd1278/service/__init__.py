221e3f22f44eb2ccfa5ba34de632343edb9ccf60
merberg <j1mb3rg@gmail.com>
2026-09-02 09:38:48 -0700

    runtime: add RX-only service package

---
+++ src/ywd1278/service/__init__.py
@@ -0,0 +1,5 @@
+"""YWD-1278 product runtime services."""
+
+from .rx_runtime import RXOnlyPacketRuntime, RXRuntimeError, RXRuntimeSnapshot
+
+__all__ = ["RXOnlyPacketRuntime", "RXRuntimeError", "RXRuntimeSnapshot"]
